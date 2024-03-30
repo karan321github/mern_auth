d159e577169e44038cd380fb90c9067aa022f233
Karan Singh <131857266+karan321github@users.noreply.github.com>
2024-03-30 18:56:25 +0530

    connect to database

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,25 @@
 import  express  from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+
+// mongoose.connect(process.env.MONGO)
+// .then(() => {
+//      console.log("database connected")   
+// }).catch((err) => {
+//    console.log('OH NO ERROR!!');
+//    console.log(err);
+// })
+
+
+mongoose.connect('mongodb://127.0.0.1:27017/mern-auth')
+const db = mongoose.connection 
+db.on("error" , console.error.bind(console , "connection error:"))
+db.once("open" , ()=> {
+        console.log("DATABASE CONNECTED")
+});
 
 const app = express();
 app.listen(3000, () => {
