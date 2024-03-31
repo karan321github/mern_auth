e03c6dff3df8992a7dee7e9826efe08fc3f5cff8
Karan Singh <131857266+karan321github@users.noreply.github.com>
2024-03-31 16:30:05 +0530

    SignIn page functionality

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,7 +1,73 @@
-
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export const SignIn = () => {
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+  //console.log(formData);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      setError(false);
+      const response = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await response.json();
+      console.log(data);
+      if (data.success === false) {
+        setError(true);
+        return;
+      }
+      navigate("/");
+    } catch (error) {
+      setLoading(false);
+      setError(false);
+    }
+  };
   return (
-    <div>SignIn</div>
-  )
-}
+    <div className="mx-auto my-10 max-w-lg p-3 border border-gray-500 rounded-md ">
+      <h1 className="font-bold text-3xl text-center my-7">Sign In</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <input
+          className="bg-slate-100 p-3 rounded-lg"
+          type="email"
+          placeholder="Email"
+          id="email"
+          name="email"
+          onChange={handleChange}
+        />
+
+        <input
+          className="bg-slate-100 p-3 rounded-lg"
+          type="password"
+          placeholder="Password"
+          id="password"
+          name="password"
+          onChange={handleChange}
+        />
+        <button className="p-3 uppercase hover:opacity-95 text-white rounded-lg bg-slate-700 ">
+          {loading ? "Loading" : "Sign In"}
+        </button>
+      </form>
+      <div className="flex gap-2 mt-5">
+        <p>Dont Have an account?</p>
+        <Link to="/sign-in">
+          <span className="text-blue-500 ">Sign up</span>
+        </Link>
+      </div>
+      <p className="text-red-700 mt-5">{error && "something went wrong"}</p>
+    </div>
+  );
+};
