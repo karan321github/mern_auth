d8451a29b0179bf73bd138e242e134916016f0fd
Karan Singh <131857266+karan321github@users.noreply.github.com>
2024-04-01 12:51:56 +0530

    create and add google authentication

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -3,7 +3,6 @@ import User from "../models/user.js";
 import { errorHandler } from "../utils/error.js";
 import jwt from "jsonwebtoken";
 
-
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPassword = bcrypt.hashSync(password, 10);
@@ -25,11 +24,56 @@ export const signin = async (req, res, next) => {
     if (!validPassword) return next(errorHandler(404, "wrong credentials"));
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
     const { password: hashedPassword, ...rest } = validUser._doc;
-    const expiryDate = new Date(Date.now() + 360000);
+    const expiryDate = new Date(Date.now() + 3600000);
     res
       .cookie("access token", token, { httpOnly: true, expires: expiryDate })
       .status(200)
       .json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const googleSignIn = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (user) {
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      const { password: hashedPassword, ...rest } = user._doc;
+      const expiryDate = new Date(Date.now() + 3600000);
+      res
+        .cookie("access token", token, {
+          httpOnly: true,
+          expires: expiryDate,
+        })
+        .status(200)
+        .json(rest);
+    } else {
+      const generatedPassword =
+        Math.random().toString(36).slice(-8) +
+        Math.random().toString(36).slice(-8);
+      const hashedPassword = bcrypt.hashSync(generatedPassword, 10);
+      const newUser = new User({
+        username:
+        req.body.name.split(' ').join('').toLowerCase() +
+        Math.random().toString(36).slice(-8),
+        email: req.body.email,
+        password: hashedPassword,
+        profile: req.body.photo,
+      });
+
+      await newUser.save();
+      const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
+      const { password: hashedPassword2, ...rest } = newUser._doc;
+      const expiryDate = new Date(Date.now() + 3600000); // 1 hour
+      res
+        .cookie("access_token", token, {
+          httpOnly: true,
+          expires: expiryDate,
+        })
+        .status(200)
+        .json(rest);
+    }
   } catch (error) {
     next(error);
   }
